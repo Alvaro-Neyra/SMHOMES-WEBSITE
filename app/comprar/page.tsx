525bfc2de8e46f9f4cec8509788f21461717fbf5
Alvaro Alberto Neyra Salazar <alvaroprogramador.neyra.salazar@gmail.com>
2025-03-18 00:02:50 -0500

    Changing some things

--- app/comprar/page.tsx
+++ app/comprar/page.tsx
@@ -1,84 +1,12 @@
-import FAQBuy from "../components/molecules/FAQBuy";
+import FAQ from "../components/molecules/FAQ";
 import FormComponent from "../components/molecules/FormComponent";
 import HeroSection from "../components/molecules/HeroSectionButtons";
 import HeroForm from "../components/organisms/HeroForm";
 import TestimonialsCarrusel from "../components/organisms/TestimonialsCarrusel";
-import { formFields } from "../utils/constants";
+import { FAQBuyItems, formFields, highlightsComprar, stats } from "../utils/constants";
 import Link from "next/link";
 
 export default function BuyPage() {
-    const stats = [
-        {
-            icon: "/home-icon.svg",
-            value: "+150",
-            label: "propiedades disponibles",
-        },
-        {
-            icon: "/star.svg",
-            value: "4,9",
-            label: "sobre 5 en Google",
-        },
-    ];
-
-    const highlights = [
-        {
-            number: 1,
-            title: "Búsqueda personalizada de vivienda",
-            description:
-                "Te ayudamos a encontrar la propiedad perfecta según tus necesidades y presupuesto. Analizamos el mercado para ofrecerte las mejores opciones.",
-            highlight: "¡Más del 95% de nuestros clientes encuentran su hogar ideal!",
-        },
-        {
-            number: 2,
-            title: "Asesoramiento financiero",
-            description:
-                "Contamos con expertos en financiación que te guiarán en todo el proceso, desde la solicitud de hipoteca hasta la firma final.",
-            highlight: "Y lo mejor… ¡asesoramiento gratuito!",
-        },
-        {
-            number: 3,
-            title: "Acceso exclusivo a propiedades premium",
-            description:
-                "Gracias a nuestra red de colaboradores, tenemos acceso a propiedades exclusivas que no están disponibles en otros portales inmobiliarios.",
-            highlight: "¡Encuentra tu hogar soñado con nosotros!",
-        },
-        {
-            number: 4,
-            title: "Confianza y tranquilidad en la compra",
-            description:
-                "Con SM HOME'S, puedes estar seguro de que estás trabajando con profesionales de confianza. Tenemos más de 500 reseñas positivas en “Google”.",
-            highlight: "¡Nuestra valoración es de 4,9 sobre 5 en “Google”!",
-        },
-        {
-            number: 5,
-            title: "Visitas virtuales y presenciales",
-            description:
-                "Ofrecemos visitas virtuales y presenciales para que puedas explorar las propiedades desde la comodidad de tu hogar o en persona.",
-            highlight: "¡Elige la opción que mejor se adapte a ti!",
-        },
-        {
-            number: 6,
-            title: "Gestión legal completa",
-            description:
-                "Nos encargamos de todo el proceso legal, desde la revisión del contrato hasta la firma final, para que no tengas que preocuparte por nada.",
-            highlight: "¡Sabemos hacer las cosas bien, puede estar tranquilo!",
-        },
-        {
-            number: 7,
-            title: "Traducción",
-            description:
-                "Nuestros agentes inmobiliarios hablan diferentes idiomas (español, inglés, francés, sueco), lo que te asegurará que siempre estarás atendido en tu idioma.",
-            highlight: "",
-        },
-        {
-            number: 8,
-            title: "Postcompra",
-            description:
-                "Una vez comprada la vivienda, nos encargaremos de realizar los cambios de titularidad de los suministros, titularidad catastral, y más.",
-            highlight: "",
-        },
-    ];
-
     return (
         <section>
             <HeroSection
@@ -106,12 +34,12 @@ export default function BuyPage() {
                 heading="En SM HOME&apos;S, eliminamos el estrés del proceso y te ofrecemos el servicio inmobiliario más completo para comprar tu propiedad de forma rápida, cómoda y al mejor precio."
                 subHeading="¿Qué hacemos para comprar tu casa en tiempo record?"
                 strongSubHeading="comprar tu casa en tiempo record"
-                otherChildren={<FAQBuy />}
+                otherChildren={<FAQ items={FAQBuyItems} />}
             >
                 <section>
                     <div className="px-4 space-y-[2vw] xl:space-y-[3vw]">
                         <div className="grid grid-cols-1 lg:grid-cols-2 gap-2 lg:gap-4">
-                            {highlights.map((highlight, index) => (
+                            {highlightsComprar.map((highlight, index) => (
                                 <div
                                     key={index}
                                     className="bg-blackSoft30 p-6 rounded-lg text-center hover:scale-105 transition-all duration-300 xl:space-y-[1vw]"
--- app/vender/page.tsx
+++ app/vender/page.tsx
@@ -3,82 +3,10 @@ import FormComponent from "../components/molecules/FormComponent";
 import HeroSection from "../components/molecules/HeroSectionButtons";
 import HeroForm from "../components/organisms/HeroForm";
 import TestimonialsCarrusel from "../components/organisms/TestimonialsCarrusel";
-import { formFields } from "../utils/constants";
+import { FAQItems, formFields, highlights, stats } from "../utils/constants";
 import Link from "next/link";
 
 export default function SellPage() {
-    const stats = [
-        {
-            icon: "/home-icon.svg",
-            value: "+100",
-            label: "casas vendidas al año",
-        },
-        {
-            icon: "/star.svg",
-            value: "4,9",
-            label: "sobre 5 en Google",
-        },
-    ];
-
-    const highlights = [
-        {
-            number: 1,
-            title: "Valoración gratuita de tu vivienda",
-            description:
-                "Ponemos a tu disposición una valoración basada en precios reales de las viviendas que se han vendido en los últimos meses en tu misma zona. Te ayudamos a definir la mejor estrategia para vender tu casa, maximizando tus posibilidades de éxito.",
-            highlight: "¡El 99% de las viviendas que tienen un precio adecuado se venden!",
-        },
-        {
-            number: 2,
-            title: "Servicios “Home Staging” profesional",
-            description:
-                "Sabemos que la primera impresión es crucial a la hora de vender una vivienda. Por eso, te ofrecemos un servicio de Home Staging profesional para que tu propiedad cause la mejor impresión posible a los compradores potenciales, aumentando las posibilidades de venta y el precio final.",
-            highlight: "Y lo mejor de todo… ¡totalmente GRATIS para usted!",
-        },
-        {
-            number: 3,
-            title: "Experiencia y resultados probados",
-            description:
-                "En Casas y Mar combinamos la tecnología más avanzada con un sistema de venta probado y eficaz, respaldado por más de 25 años de experiencia en el sector inmobiliario de Torrevieja y la Costa Blanca Sur. Contamos con una red de más de 400 colaboradores a nivel local, nacional e internacional.",
-            highlight: "¡Vendemos más de 100 viviendas de media al año!",
-        },
-        {
-            number: 4,
-            title: "Confianza y tranquilidad en la venta",
-            description:
-                "Con Casas y Mar, puedes estar seguro de que tu propiedad está en buenas manos. Tenemos más de 500 reseñas sobre nosotros en “Google”, con una valoración de 4,9 sobre 5, lo que significa que el grado de satisfacción de los clientes con nuestro servicio es muy alto. Búscanos y descúbrelo por ti mismo lo que otros clientes opinan de nosotros.",
-            highlight: "¡Nuestra valoración es de 4,9 sobre 5 en “Google”!",
-        },
-        {
-            number: 5,
-            title: "Máxima difusión",
-            description:
-                "Realizaremos un reportaje fotográfico profesional y un TOUR VIRTUAL. Publicaremos tu vivienda en los mejores y más visitados portales inmobiliarios de España y del extranjero, especializados en la zona, sin coste alguno. La iremos rotando en los mejores puestos destacados para lograr mayor impacto, igual que en nuestro escaparate a pie de calle de nuestra oficina en Torrevieja.",
-            highlight: "¡Cuanta más visibilidad, más rápido se vende!",
-        },
-        {
-            number: 6,
-            title: "Servicio Notaría",
-            description:
-                "Este paso burocrático puede resultar un tanto complejo, por lo que nuestros agentes inmobiliarios te asesorarán en todo momento. El día de la firma de la escritura, te acompañará una persona del bufete de abogados con el que trabajamos para que revise todo el proceso y pueda resolver cualquier duda, incluso si no puedes venir ese día, nosotros podemos representarte y ocuparnos de todo con un poder notarial.",
-            highlight: "¡Sabemos hacer las cosas bien, puede estar tranquilo!",
-        },
-        {
-            number: 7,
-            title: "Traducción",
-            description:
-                "Nuestros agentes inmobiliarios hablan diferentes idiomas (español, inglés, francés, sueco), lo que te asegurará que siempre estarás atendido en tu idioma para que puedas sentirte cómodo y tranquilo en todo el proceso de venta de su vivienda.",
-            highlight: "",
-        },
-        {
-            number: 8,
-            title: "Postventa",
-            description:
-                "Una vez firmada la escritura de venta de la vivienda, nos encargaremos de realizar también los cambios de titularidad de los suministros, titularidad catastral, titularidad de la comunidad, solicitud de plusvalía, etc.",
-            highlight: "",
-        },
-    ];
-
     return (
         <section>
             <HeroSection
@@ -101,7 +29,7 @@ export default function SellPage() {
                     sellSection={true}
                 />
             </HeroSection>
-            <HeroForm id="sell-reasons-section" heading="En SM HOME&apos;S, eliminamos el estrés del proceso y te ofrecemos el servicio inmobiliario más completo para vender tu propiedad de forma rápida, cómoda y al mejor precio." subHeading="¿Qué hacemos para vender tu casa en tiempo record?" strongSubHeading="vender tu casa en tiempo record" otherChildren={<Faq />} sellSection={true}>
+            <HeroForm id="sell-reasons-section" heading="En SM HOME&apos;S, eliminamos el estrés del proceso y te ofrecemos el servicio inmobiliario más completo para vender tu propiedad de forma rápida, cómoda y al mejor precio." subHeading="¿Qué hacemos para vender tu casa en tiempo record?" strongSubHeading="vender tu casa en tiempo record" otherChildren={<Faq items={FAQItems}/>} sellSection={true}>
                 <section>
                     <div className="px-4 space-y-[2vw] xl:space-y-[3vw]">
                         <div className="grid grid-cols-1 lg:grid-cols-2 gap-2 lg:gap-4">
